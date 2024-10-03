7b387db9772df102f408df9ff95bb6838878cd5e
sushma <118838016+korlapatisushma@users.noreply.github.com>
2024-10-03 23:04:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ region="eu-north-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = "t2.micro"
+  ami           = "ami-0129bfde49ddb0ed6"
+  instance_type = "t3.micro"
   key_name      = "sas"
   tags = {
     Name = " rahaminstance"
