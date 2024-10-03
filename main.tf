31442f3a8d6e214bc59f5e45eef76d82cd02b164
sushma <118838016+korlapatisushma@users.noreply.github.com>
2024-10-03 22:44:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region="eu-north-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-04823729c75214919"
+  ami           = "ami-0ebfd941bbafe70c6"
   instance_type = "t2.micro"
   key_name      = "yterraform"
   tags = {
